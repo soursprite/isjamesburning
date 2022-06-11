b27a0536d60280dd9a43ff27b89b7f8cd5aa3eed
James <jemsgold21@gmail.com>
2022-06-11 02:22:36 -0500

    trying promis

--- app.js
+++ app.js
@@ -7,47 +7,50 @@ const app = express()
 const cors = require('cors')
 const PORT = 8000;
 const ejs = require ('ejs')
+const { resolve } = require('path')
 
 let currentStatus
 
 //national weather service requires 2 API requests
-axios.get('https://api.weather.gov/points/40.1047,-88.2243', {headers: {'User-Agent': 'isJamesBurning.netlify.app/contact:jemsgold21@gmail.com'}}) //first API req
-.then(response => {
-    console.log(JSON.stringify(response.data));
-    const dataJSON = response.data
+const weatherGet = () => {
+    axios.get('https://api.weather.gov/points/40.1047,-88.2243', {headers: {'User-Agent': 'isJamesBurning.netlify.app/contact:jemsgold21@gmail.com'}}) //first API req
+    .then(response => {
+        console.log(JSON.stringify(response.data));
+        const dataJSON = response.data
 
-    //second API call
-    return axios.get(dataJSON.properties.forecastHourly, {headers: {'User-Agent': 'isJamesBurning.netlify.app/contact:jemsgold21@gmail.com'}}) //2nd API req using forecastHourly API url
-})
-.then(response2 => {
-    const dataPoints = response2.data.properties.periods[0]
-    /* for (const prop in dataPoints) {
-        console.log(prop,dataPoints[prop])
-    } */
-    currentStatus = status(dataPoints.temperature);
-})
-.catch(error => {
-    console.error(error + " SHIT");
-}); 
+        //second API call
+        return axios.get(dataJSON.properties.forecastHourly, {headers: {'User-Agent': 'isJamesBurning.netlify.app/contact:jemsgold21@gmail.com'}}) //2nd API req using forecastHourly API url
+    })
+    .then(response2 => {
+        const dataPoints = response2.data.properties.periods[0]
+        /* for (const prop in dataPoints) {
+            console.log(prop,dataPoints[prop])
+        } */
+        currentStatus = status(dataPoints.temperature);
+    })
+    .catch(error => {
+        console.error(error + " SHIT");
+    }); 
 
-const status = (temp) => {
-    return (`James is ${
-        (temp > 120) ? "ashes" :
-        (temp > 100) ? "fire incarnate" :
-        (temp > 89) ? "burning" :
-        (temp > 84) ? "melting" :
-        (temp > 78) ? "sweating" :
-        (temp > 72) ? "warm" :
-        (temp > 69) ? "vibin'" :
-        (temp === 69) ? "nice" :
-        (temp > 64) ? "chillin'" :
-        (temp > 58) ? "cool" :
-        (temp > 45) ? "cold" :
-        (temp > 38) ? "shivering" :
-        (temp > 20) ? "freezing" :
-        (temp > 5) ? "frozen" :
-        (temp > -10) ? "Mr. Freeze" : "superconducting"
-    }`)
+    const status = (temp) => {
+        return (`James is ${
+            (temp > 120) ? "ashes" :
+            (temp > 100) ? "fire incarnate" :
+            (temp > 89) ? "burning" :
+            (temp > 84) ? "melting" :
+            (temp > 78) ? "sweating" :
+            (temp > 72) ? "warm" :
+            (temp > 69) ? "vibin'" :
+            (temp === 69) ? "nice" :
+            (temp > 64) ? "chillin'" :
+            (temp > 58) ? "cool" :
+            (temp > 45) ? "cold" :
+            (temp > 38) ? "shivering" :
+            (temp > 20) ? "freezing" :
+            (temp > 5) ? "frozen" :
+            (temp > -10) ? "Mr. Freeze" : "superconducting"
+        }`)
+    }
 }
 
 
@@ -55,9 +58,16 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (request, res) => {
-    res.render(__dirname + '/index', {
-        currentStatus: currentStatus
-    });
+
+    const startPromise = new Promise ((res, rej) => {
+        weatherGet()
+        resolve()
+    })
+    .then(() => {
+        res.render(__dirname + '/index', {
+            currentStatus: currentStatus
+        });
+    })
 });
 
 app.listen(process.env.PORT || PORT, () => {
